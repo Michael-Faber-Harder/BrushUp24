95ee8bbe90e34f300c68d6345c6191404f83ca9e
Michael Faber Harder <lizvanzanen@gmail.com>
2021-09-05 12:09:26 +0200

    Add project files.

---
+++ BrushUp23/Student.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Brushup_Student
+{
+	public class Student
+	{
+		public enum Gender
+		{
+			Male = 0,
+			Female = 1
+		};
+
+		#region Instancefields
+		private string _name;
+		private string _adress;
+		private int _semester;
+		private Gender _studentGender;
+		#endregion
+
+
+		public Student(string name, string adress, int semester, Gender studentGender)
+		{
+			_name = name;
+			_adress = adress;
+			_semester = semester;
+			_studentGender = studentGender;
+		}
+
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+
+		#region Properties
+		public string Adress
+		{
+			get { return _adress; }
+			set
+			{
+				if (value.Equals(null))
+				{
+					throw new NullReferenceException(("Adress cannot be null"));
+				}
+				_adress = value;
+			}
+		}
+
+		public string Name
+		{
+			get { return _name; }
+			set
+			{
+				if (value.Length < 2)
+				{
+					throw new ArgumentOutOfRangeException("Name must be at least 2 characters");
+				}
+				_name = value;
+			}
+		}
+
+		public int Semester
+		{
+			get { return _semester; }
+			set
+			{
+				if (value < 1 || value > 8)
+				{
+					throw new Exception("Semester must be 1-8");
+				}
+				_semester = value;
+			}
+		}
+
+		public Gender StudentGender
+		{
+			get { return _studentGender; }
+			set { _studentGender = value; }
+		}
+		#endregion
+	}
+}
---
+++ BrushUp23Tests/StudentTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brushup_Student;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brushup_Student.Tests
+{
+	[TestClass()]
+	public class StudentTests
+	{
+		//private Student st;
+
+		//[TestInitialize]
+		//public void Setup()
+		//{
+		Student st = new Student("Michael", "Slagelse", 3, Student.Gender.Male);
+		//}
+
+		[TestMethod()]
+		public void ConstructorTest()
+		{
+			Assert.AreEqual("Michael", st.Name);
+			Assert.AreEqual("Slagelse", st.Adress);
+			Assert.AreEqual(3, st.Semester);
+			Assert.AreEqual(Student.Gender.Male, st.StudentGender);
+		}
+
+		[TestMethod()]
+		public void NameTest()
+		{
+			st.Name = "Julemand";
+			Assert.AreEqual("Julemand", st.Name);
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => st.Name = "N");
+
+		}
+
+		[TestMethod()]
+		public void AdressTest()
+		{
+			Assert.ThrowsException<NullReferenceException>(() => st.Adress = null);
+		}
+
+
+
+		[TestMethod()]
+		public void ToStringTest()
+		{
+		}
+	}
+}
